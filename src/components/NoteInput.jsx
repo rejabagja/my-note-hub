feb8e579b65fbbee30dab581a735b706c5619bf3
Reza Subagja <rezasubagja5@gmail.com>
2024-11-28 10:34:09 +0700

    feat: refactor code in title length reminder feature

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -7,9 +7,8 @@ class NoteInput extends React.Component {
     this.state = {
       title: '',
       body: '',
+      maxTitleLength: 50,
     };
-
-    this.maxTitleLength = 50;
 
     this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
     this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
@@ -17,8 +16,10 @@ class NoteInput extends React.Component {
   }
 
   onTitleChangeEventHandler(event) {
-    if (event.target.value.length <= this.maxTitleLength) {
-      this.setState({ title: event.target.value });
+    const { value } = event.target;
+    const { maxTitleLength } = this.state;
+    if (value.length <= maxTitleLength) {
+      this.setState({ title: value });
     }
   }
 
@@ -38,11 +39,14 @@ class NoteInput extends React.Component {
   }
 
   render() {
+    const { title, maxTitleLength } = this.state;
+    const reminingChars = maxTitleLength - title.length;
+    
     return (
       <div className="note-input">
         <h2>Buat Catatan</h2>
         <form method="post" onSubmit={this.onSubmitEventHandler}>
-          <p className="note-input__title__char-limit" style={this.state.title.length >= 40 ? { color: "#ff5f52"} : null}>Sisa karakter: {this.maxTitleLength - this.state.title.length}</p>
+          <p className="note-input__title__char-limit" style={reminingChars <= 10 ? { color: "#ff5f52"} : null}>Sisa karakter: {reminingChars}</p>
           <input type="text" className="note-input__title" placeholder="Ini adalah judul ..." value={this.state.title} onChange={this.onTitleChangeEventHandler} required />
           <textarea className="note-input__body" placeholder="Tuliskan catatanmu di sini ..." required value={this.state.body} onChange={this.onBodyChangeEventHandler}></textarea>
           <button type="submit">Submit</button>
